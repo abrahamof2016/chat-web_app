17bf1f758964ab23a0f346aecb2a42c1093fecef
Abraham <jarageda9@gmail.com>
2024-04-23 00:40:53 +0300

    Update server.js

--- chat-backend/server.js
+++ chat-backend/server.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
  
 // MongoDB Connection
-mongoose.connect("mongodb://127.0.0.1[:27017]", {
+mongoose.connect("mongodb://127.0.0.1[27017]", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
